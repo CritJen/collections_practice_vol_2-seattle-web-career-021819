c875ec804315e9f6187b2be24451cba9d463b178
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-17 23:17:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,7 +35,11 @@ end
 def merge_data(array1, array2)
   final_array = []
   array1.each do |person|
-    
+    first_name = person[:first_name]
+    array2_ppl = array2[0]
+    array2_person  = array2_ppl[first_name] 
+     mid_hash = {person, array2_person}
+     final_array << mid_hash
   end
 end
 
