ae861130f9d97c03c8b14d158255a7b4e125bdd3
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-18 00:12:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -61,7 +61,7 @@ def organize_schools(schools)
     location_hash = school[1]
     school_location = location_hash.values.join
     if org_schools.has_key?(school_location)
-       org_school[:school_location] = school
+       org_school[school_location] = school_name
        return "poop"
     else
       org_schools[school_location] = school_name
