b2bce8a5b76baa595fb55ac4e75173c568e4a0da
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-18 00:06:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -59,7 +59,7 @@ def organize_schools(schools)
   schools.each do |school|
     location_hash = school[1]
     school_location = location_hash.values
-    if org_schools.has_key?(school_location[0])
+    if org_schools.has_key?(:school_location)
        org_school[:school_location] = school
     else
       org_schools[school_location] = school
